0f1866b82dd19fea3b8c38e56b7fb9e3ae8151cc
Tobias <tobiaso@student.eth.ch>
2022-03-08 23:02:59 +0100

    Updates

---
+++ precache-manifest.e4da1f607d2d0489087476ca644b9af2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4511f69b5716dc0856306971c9ba1d8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0a5f145b696ceffa2f2",
+    "url": "/static/css/main.55119997.chunk.css"
+  },
+  {
+    "revision": "1af9b7aecef2ffa39dba",
+    "url": "/static/js/2.00d403a3.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.00d403a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0a5f145b696ceffa2f2",
+    "url": "/static/js/main.9ea2da7a.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "dbcd28f3690f8377749bab53ebd66fa6",
+    "url": "/static/media/resume.dbcd28f3.pdf"
+  }
+]);
